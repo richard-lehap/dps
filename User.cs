64d2d54d1fca25f65cc57432b6214086a79f1139
Richardo <iloveunixandbsd@gmail.com>
2017-02-12 00:03:19 -0500

    Add User repository.

--- User.cs
+++ User.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
+
 namespace Dps {
     public class User {
         public string Key { get; }
         public string FirstName { get; }
         public string LastName { get; }
         public int Age { get; }
+    }
+
+    public interface IUserRepository {
+        void Add(User user);
+        IEnumerable<User> GetAll();
     }
 }
