26dd45107ec8feb0643aa26ebc7165cbc95c0630
Richardo <iloveunixandbsd@gmail.com>
2017-02-11 23:49:31 -0500

    Add User model.

---
+++ User.cs
@@ -0,0 +1,8 @@
+namespace Dps {
+    public class User {
+        public string Key { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+    }
+}
